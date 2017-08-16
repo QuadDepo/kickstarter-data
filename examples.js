67ad54b5e305366938f13178c6d55440b19a66e2
Ryan George <23100@ma-web.nl>
2017-08-16 21:00:53 +0200

    Working updates of kickstarter page. Promises are working and getting information of all subpages

--- examples.js
+++ examples.js
@@ -1,16 +1,15 @@
 // Import kickstarter-data
-const KS = require('./index.js');
+const KS = require(['./index.js']);
 
 // options of kickstarter-data
 var options = {
-	url: "https://www.kickstarter.com/projects/lumaboards/luma-boards-the-most-futuristic-cruiser-ever-creat/updates",
+	url: "https://www.kickstarter.com/projects/lumaboards/luma-boards-the-most-futuristic-cruiser-ever-creat/",
 }
 // Creating new KS project with options
 var project = new KS.project(options);
 
 // Calling all function using .all()
-
-project.all(function(error, data){
+project.updates(function(error, data){
 	// if there is an error
 	if (error) {
 		// error
